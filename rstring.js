e5fc53e696a149b277c5301636b187dee09b3ef4
goldenstein64 <goldnstein64@gmail.com>
2023-02-16 21:51:22 -0500

    Made help message better

--- rstring.js
+++ rstring.js
@@ -3,13 +3,13 @@ const { ArgumentParser } = require("argparse")
 const array = (size, val) => Array(size).fill(val)
 const choose = (choices) => choices.at(choices.length * Math.random())
 
-const parser = new ArgumentParser({ description: "Generate a random string." })
+const parser = new ArgumentParser({ prog: "rstring", description: "Generate a random string." })
 
-parser.add_argument("size", { type: parseInt, nargs: "?" })
-parser.add_argument("-u", { action: "store_true", dest: "upper" })
-parser.add_argument("-l", { action: "store_true", dest: "lower" })
-parser.add_argument("-n", { action: "store_true", dest: "numeric" })
-parser.add_argument("-s", { action: "store_true", dest: "special" })
+parser.add_argument("size", { type: parseInt, nargs: "?", help: "number of characters to output; defaults to 12-30 chars" })
+parser.add_argument("-u", { action: "store_true", dest: "upper", help: "include uppercase" })
+parser.add_argument("-l", { action: "store_true", dest: "lower", help: "include lowercase" })
+parser.add_argument("-n", { action: "store_true", dest: "numeric", help: "include numbers" })
+parser.add_argument("-s", { action: "store_true", dest: "special", help: "include special characters" })
 
 const args = parser.parse_args()
 
@@ -36,7 +36,7 @@ const keys = Object.keys(strings)
 
 const includedKeys = keys.some(k => args[k]) ? keys.filter(k => args[k]) : keys
 
-const pool = includedKeys.map(k => strings[k]).reduce((a, b) => a.concat(b))
+const pool = includedKeys.flatMap(k => strings[k])
 
 const result = array(size).map(() => choose(pool))
 console.log(result.join(""))
